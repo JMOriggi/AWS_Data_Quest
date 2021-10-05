60340302ae4df159292423fbf8f7fd41364c0014
JMOriggi <36966882+JMOriggi@users.noreply.github.com>
2021-10-05 17:19:54 -0400

    Update iam.tf

--- Data_Pipeline_Infrastructure/iam.tf
+++ Data_Pipeline_Infrastructure/iam.tf
@@ -2,11 +2,11 @@ resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy"
   role = "${aws_iam_role.lambda_role.id}"
 
-  policy = "${file("policy/lambda_policy.json")}"
+  policy = "${file("policies/lambda_policy.json")}"
 }
 
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
 
-  assume_role_policy = "${file("policy/lambda_assume_policy.json")}"
+  assume_role_policy = "${file("policies/lambda_assume_policy.json")}"
 }
